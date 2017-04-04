6e5b15f3edd806e31c34a49fcaf42a3a66c381b5
daniel.rocha <danielsantosr.rj@gmail.com>
2017-04-04 14:09:52 -0300

    start

---
+++ src/main/java/com/gank/StudyGroovySpockApplication.java
@@ -0,0 +1,12 @@
+package com.gank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGroovySpockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGroovySpockApplication.class, args);
+	}
+}
